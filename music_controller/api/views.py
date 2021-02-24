ae04231749d719b6f930bb1c5d11b6887cfc0fa6
Vergangenheit <lorenzo.ostano@gmail.com>
2021-02-24 08:27:08 +0100

    part7

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -6,6 +6,7 @@ from .models import Room
 from django.db.models.query import QuerySet
 from django.http import HttpRequest
 from rest_framework.views import APIView
+from rest_framework.utils.serializer_helpers import ReturnDict
 
 # Create your views here.
 
@@ -13,6 +14,25 @@ from rest_framework.views import APIView
 class RoomView(generics.ListAPIView):
     queryset: QuerySet = Room.objects.all()
     serializer_class = RoomSerializer 
+
+class GetRoom(APIView):
+    serializer_class = RoomSerializer
+    lookup_url_kwarg: str = 'code'
+
+    def get(self, request: HttpRequest, format=None) -> Response:
+        code: str = request.GET.get(self.lookup_url_kwarg)
+        if code != None:
+            room: QuerySet = Room.objects.filter(code=code)
+            if len(room) > 0:
+                data: ReturnDict = RoomSerializer(room[0]).data
+                data['is_host'] = self.request.session.session_key == room[0].host
+                return Response(data, status=status.HTTP_200_OK)
+            return Response({'Room Not Found': 'Invalid Room Code.'}, status=status.HTTP_404_NOT_FOUND)
+
+        return Response({'Bad Request': 'Code paramater not found in request'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
 
 class CreateRoomView(APIView):
     serializer_class = CreateRoomSerializer
